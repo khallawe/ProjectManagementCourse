9882251a5141617a149ad9939970fc835be37900
khallawe <khallawe@gmail.com>
2017-04-19 12:04:28 -0500

    no message

---
+++ OTS/OTS.DAL/OTS_DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using OTS.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OTS.DAL
+{
+    class OTS_DBConnection:DbContext
+    {
+        public DbSet<Answer> answers { get; set; }
+    }
+}
